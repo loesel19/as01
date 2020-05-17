a03a75421d277e5a45c66554cb1dba075d1f750b
loesel19 <56932273+loesel19@users.noreply.github.com>
2020-05-16 21:42:17 -0400

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your favorite number is <?php echo $_GET["numb"];?><br>
+Your favorite car is: <?php echo $_GET["car"]; ?>
+
+</body>
+</html>
